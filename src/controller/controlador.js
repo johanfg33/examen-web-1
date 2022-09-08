38bad14943eafd0a8d4aadacf0c4b853a8931fa6
505 <505@academicos.cesde.edu.co>
2022-09-07 20:25:12 -0500

    examen

---
+++ src/controller/controlador.js
@@ -0,0 +1,321 @@
+let peliculas = [
+    {
+        nombre:"rapidos y furiosos",
+        genero:"Accion",
+        duracion:210,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cine-miercoles.appspot.com/o/rapidos.jpg?alt=media&token=ca1865ec-b237-40f3-9d0f-e723822c7154",
+        sipnosis:"Lo primero es la familia",
+        clasificacion:"+16",
+        idioma:"Español",
+        director:"Robert Cohen",
+        actores:"paul walker, toretto"
+    },
+    {
+        nombre:"Dragon ball Super",
+        genero:"Anime",
+        duracion:120,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cine-miercoles.appspot.com/o/dragon-ball-super-super-hero.webp?alt=media&token=02819480-ae99-47e6-bc5d-ca9c7bcf641f",
+        sipnosis:"Batallas epicas",
+        clasificacion:"todas las edades",
+        idioma:"Japones",
+        director:"Tetsuro Kodama",
+        actores:"goku, vegeta, gohan"
+    },
+    {
+        nombre:"Megamente",
+        genero:"Infantil",
+        duracion:130,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cine-miercoles.appspot.com/o/Megamente.webp?alt=media&token=46033164-af30-4216-885d-59684540c04c",
+        sipnosis:"Un villano que tiene el poder de su mega mente",
+        clasificacion:"todas las edades",
+        idioma:"Español",
+        director:"Tom McGrath",
+        actores:"megamente, el heroe"
+    },
+    {
+        nombre:"jason vs freddy",
+        genero:"Terror",
+        duracion:200,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cine-miercoles.appspot.com/o/91Tg4LAlMML._RI_.jpg?alt=media&token=45d70c61-1d60-4158-9e06-2f581811e419",
+        sipnosis:"Una pelea entre villanos",
+        clasificacion:"+18",
+        idioma:"Español",
+        director:"johs libert",
+        actores:"freddy, jason, la chica rubia"
+    },
+    {
+        nombre:"Spiderman",
+        genero:"Ficcion",
+        duracion:250,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cine-miercoles.appspot.com/o/81i9V1jlCsL._AC_SL1500_.jpg?alt=media&token=103e7742-f400-4ce2-953c-bbcd9b2c8bfb",
+        sipnosis:"Se reunen todos los spiderman de los universos",
+        clasificacion:"todas las edades",
+        idioma:"Español",
+        director:"Tom michigan",
+        actores:"pitter parker1, pitter parker2, pitter parker3"
+    },
+    {
+        nombre:"Avengers",
+        genero:"Ficcion",
+        duracion:300,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cine-miercoles.appspot.com/o/81ai6zx6eXL._AC_SL1304_.jpg?alt=media&token=e72c7e61-f7be-48a5-9799-a96928f06d3b",
+        sipnosis:"Los avengers salvan el universo",
+        clasificacion:"todas las edades",
+        idioma:"Español",
+        director:"Iroman",
+        actores:"tanos, spiderman, capitan america"
+    },
+    {
+        nombre:"Cars",
+        genero:"Infantil",
+        duracion:130,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cine-miercoles.appspot.com/o/435900.jpg?alt=media&token=07a9eb95-623e-4c0e-acb6-72464065cfe7",
+        sipnosis:"La carrera de los carros con vida",
+        clasificacion:"todas las edades",
+        idioma:"Español",
+        director:"Tom McGrath",
+        actores:"cars, el viejo, etc"
+    },
+    {
+        nombre:"El exorcista",
+        genero:"Terror",
+        duracion:150,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cine-miercoles.appspot.com/o/450_1000.jpg?alt=media&token=a42832cf-cd9a-4f95-9f02-bd61de82c0c4",
+        sipnosis:"El exorcisma de una niña muy tenebrosa",
+        clasificacion:"+18",
+        idioma:"Español",
+        director:"Alis malthor",
+        actores:"la niña, el exorcista, el demonio"
+    },
+    {
+        nombre:"Harry potter",
+        genero:"Fantasia",
+        duracion:200,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cine-miercoles.appspot.com/o/19765356.webp?alt=media&token=a92e3cc4-b66b-45df-9680-54e69c93bcbf",
+        sipnosis:"La bat6alla de los hechizeros mas fuertes",
+        clasificacion:"+13",
+        idioma:"Español",
+        director:"carls motsfyie",
+        actores:"harry, germayoni, ron"
+    },
+    {
+        nombre:"avatar",
+        genero:"Fantasia",
+        duracion:130,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cine-miercoles.appspot.com/o/12b954ac4a30f15d1fe3f207675a5481.jpg?alt=media&token=7c82bdab-036c-4991-b31f-3eb652c6b518",
+        sipnosis:"Visitando un mundo maravilloso",
+        clasificacion:"+13",
+        idioma:"Español",
+        director:"James Cameron",
+        actores:"los aliens, las bestias, etc"
+    }
+]
+
+peliculas_estreno = [
+    {
+        nombre:"titanic",
+        genero:"Amor",
+        duracion:130,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cine-miercoles.appspot.com/o/1.jpg?alt=media&token=531ca193-37e4-4409-bea7-10ceff82fd29",
+        sipnosis:"Un amor debajo del mar muy triste",
+        clasificacion:"+13",
+        idioma:"Español",
+        director:"lisa simon",
+        actores:"la chica, el chico, el titanic"
+    },
+    {
+        nombre:"2012",
+        genero:"Acccion",
+        duracion:230,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cine-miercoles.appspot.com/o/2.jpg?alt=media&token=48bad1d4-b174-46e0-96bc-d6e11a654809",
+        sipnosis:"Visitando un mundo maravilloso",
+        clasificacion:"+18",
+        idioma:"Español",
+        director:"fins mundis",
+        actores:"cameros, la chica"
+    },
+    {        
+    nombre:"spirit",
+    genero:"Infantil",
+    duracion:130,
+    poster:"https://firebasestorage.googleapis.com/v0/b/cine-miercoles.appspot.com/o/3.webp?alt=media&token=78866efc-052a-4acc-90cc-a8ccee27ac1f",
+    sipnosis:"La historia de un caballo",
+    clasificacion:"todas las edades",
+    idioma:"Español",
+    director:"lions holan",
+    actores:"el caballo, el vaquero, el indio"},
+    {        
+        nombre:"project x",
+        genero:"comedia",
+        duracion:130,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cine-miercoles.appspot.com/o/5.jpg?alt=media&token=71e0cfd2-1793-4add-97ea-550a58d4b32b",
+        sipnosis:"Visitando un mundo maravilloso",
+        clasificacion:"+13",
+        idioma:"Español",
+        director:"James Cameron",
+        actores:"los aliens, los enanos, el perro"},
+    {
+        nombre:"dr stronge",
+        genero:"Accion",
+        duracion:130,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cine-miercoles.appspot.com/o/4.jpg?alt=media&token=307e0c1f-21c3-4685-bf6c-3bac4c5425d2",
+        sipnosis:"Visitando universos paralelos",
+        clasificacion:"+13",
+        idioma:"Español",
+        director:"Marvel",
+        actores:"dr strange, la bruja"
+    }
+]
+
+let fila=document.getElementById("fila")
+
+peliculas.forEach(function(pelicula){
+    
+    //console.log(pelicula)
+    console.log(pelicula.nombre)
+    console.log(pelicula.genero)
+    console.log(pelicula.duracion)
+    console.log(pelicula.poster)
+    console.log(pelicula.sinopsis)
+    console.log(pelicula.clasificacion)
+    console.log(pelicula.idioma)
+    console.log(pelicula.director)
+    console.log(pelicula.actores)
+
+
+//TRAVERSING
+    //1. tarjetas
+    let columna=document.createElement("div")
+    columna.classList.add("col")
+
+    //2. columnas
+    let tarjeta=document.createElement("div")
+    tarjeta.classList.add("card","h-100")
+
+    //Foto
+    let poster=document.createElement("img")
+    poster.classList.add("card-img-top")
+    poster.src=pelicula.poster
+
+    //nombre
+    let nombre = document.createElement('h3')
+    nombre.classList.add('card-title','text-center')
+    nombre.textContent = pelicula.nombre      
+    
+    //genero
+    let genero = document.createElement("h5")
+    genero.classList.add("text-start")
+    genero.textContent = "Genero: "+pelicula.genero
+
+    //idioma
+    let idioma=document.createElement("h6")
+    idioma.classList.add("fw-bold")
+    idioma.textContent=pelicula.idioma
+
+    //sipnosis
+    let sinopsis=document.createElement("p")
+    sinopsis.classList.add("text-start")
+    sinopsis.textContent=pelicula.sipnosis
+
+    //director
+    let director=document.createElement("h6")
+    director.classList.add("text-start")
+    director.textContent="Director:" + pelicula.director
+
+    //Actores
+    let actores=document.createElement("h6")
+    actores.classList.add("text-start")
+    actores.textContent="Actores:" + pelicula.actores
+
+    //Padres e Hijos
+    tarjeta.appendChild(poster)
+    tarjeta.appendChild(nombre)
+    tarjeta.appendChild(genero)
+    tarjeta.appendChild(idioma)
+    tarjeta.appendChild(sinopsis)
+    tarjeta.appendChild(director)
+    tarjeta.appendChild(actores)
+    
+    columna.appendChild(tarjeta)
+    fila.appendChild(columna)
+})
+
+
+
+
+
+let fila2=document.getElementById("fila2")
+
+peliculas_estreno.forEach(function(pelicula){
+    
+    //console.log(pelicula)
+    console.log(pelicula.nombre)
+    console.log(pelicula.genero)
+    console.log(pelicula.duracion)
+    console.log(pelicula.poster)
+    console.log(pelicula.sinopsis)
+    console.log(pelicula.clasificacion)
+    console.log(pelicula.idioma)
+    console.log(pelicula.director)
+    console.log(pelicula.actores)
+
+
+//TRAVERSING
+    //1. tarjetas
+    let columna=document.createElement("div")
+    columna.classList.add("col")
+
+    //2. columnas
+    let tarjeta=document.createElement("div")
+    tarjeta.classList.add("card","h-100")
+
+    //Foto
+    let poster=document.createElement("img")
+    poster.classList.add("card-img-top")
+    poster.src=pelicula.poster
+
+    //nombre
+    let nombre = document.createElement('h3')
+    nombre.classList.add('card-title','text-center')
+    nombre.textContent = pelicula.nombre      
+    
+    //genero
+    let genero = document.createElement("h5")
+    genero.classList.add("text-start")
+    genero.textContent = "Genero: "+pelicula.genero
+
+    //idioma
+    let idioma=document.createElement("h6")
+    idioma.classList.add("fw-bold")
+    idioma.textContent=pelicula.idioma
+
+    //sipnosis
+    let sinopsis=document.createElement("p")
+    sinopsis.classList.add("text-start")
+    sinopsis.textContent=pelicula.sipnosis
+
+    //director
+    let director=document.createElement("h6")
+    director.classList.add("text-start")
+    director.textContent="Director:" + pelicula.director
+
+    //Actores
+    let actores=document.createElement("h6")
+    actores.classList.add("text-start")
+    actores.textContent="Actores:" + pelicula.actores
+
+    //Padres e Hijos
+    tarjeta.appendChild(poster)
+    tarjeta.appendChild(nombre)
+    tarjeta.appendChild(genero)
+    tarjeta.appendChild(idioma)
+    tarjeta.appendChild(sinopsis)
+    tarjeta.appendChild(director)
+    tarjeta.appendChild(actores)
+    
+    columna.appendChild(tarjeta)
+    fila2.appendChild(columna)
+})
+
+
+
